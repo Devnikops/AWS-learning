816711e30149eca4b85a9d0035c7a67848aa5ad3
Devnikops <nikks.geeks@gmail.com>
2023-10-18 11:43:08 +0530

    lambda function

---
+++ lambda_function.py
@@ -0,0 +1,20 @@
+import boto3
+import json
+
+s3 = boto3.resource('s3')
+
+def lambda_handler (event, context):
+ bucket = s3.Bucket('devops-proj2-bucket')
+ dest_bucket=s3.Bucket('devops-proj2-bucket-backup')
+
+ print(dest_bucket)
+ print(bucket)
+ 
+ for obj in bucket.objects.filter(Prefix='images/',Delimiter='/'):
+  dest_key=obj.key
+  print(dest_key)
+  print('copy file ' + dest_key)
+  s3.Object(dest_bucket.name, dest_key).copy_from(CopySource= {'Bucket': obj.bucket_name, 'Key': obj.key})
+  print('delete file from source bucket ' + dest_key)
+  s3.Object(bucket.name, obj.key).delete()
+
